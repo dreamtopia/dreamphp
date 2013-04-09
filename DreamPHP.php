e87351aa61adf850c02a849420d845b84473d7be
易鹏程 <yipengcheng1991@gmail.com>
2013-04-09 16:05:27 +0800

    first

---
+++ DreamPHP.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 框架入口文件
+ * @author 易鹏程<yipengcheng1991@gmail.com>
+ * @link http://blog.kan2020.com/
+ * @copyright 2013 
+ * @license http://dreamphp.kan2020.com/license/
+ * @since 1.0
+ */
+ 
+ //设置时区
+ date_default_timezone_set("PRC");
+ 
+ //设置框架运行开始时间
+ define('DREAM_BEGIN_TIME',microtime);
+ 
+ //设置框架目录
+ define('DREAM_PATH',dir);
+ 
