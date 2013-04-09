da6a0260873b8ce72e0c36667ba4c8668c4cd0eb
易鹏程 <yipengcheng1991@gmail.com>
2013-04-09 18:05:50 +0800

    3

---
+++ Config/Config.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 框架配置文件
+ * @author 易鹏程<yipengcheng1991@gmail.com>
+ * @link http://blog.kan2020.com/
+ * @copyright 2013 
+ * @license http://dreamphp.kan2020.com/license/
+ * @since 1.0
+ */
+ 
+ return array(
+ 	'DEBUG' => false, //调试是否开启
+ 		
+ );
--- DreamPHP.php
+++ DreamPHP.php
@@ -20,10 +20,36 @@
  //设置框架核心文件目录
  defined('DREAM_CORE_PATH') or define('DREAM_CORE_PATH',DREAM_PATH.'Core/');
  
+ //设置框类库目录
+ defined('DREAM_LIBRARIES_PATH') or define('DREAM_LIBRARIES_PATH',DREAM_PATH.'Libraries/');
+ 
  //设置项目的默认名称,如果没有设置则使用当前文件名来作为项目名称
  defined('PROJECT_NAME') or define('PROJECT_NAME', ucfirst(pathinfo($_SERVER['SCRIPT_FILENAME'],PATHINFO_FILENAME)));
  
- //设置项目目录路径,如果没有则使用当前文件所在的路径加项目名称作为项目目录
+ //设置项目目录路径,如果没有设置则使用当前文件所在的路径加项目名称作为项目目录
  defined('PROJECT_PATH') or define('PROJECT_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/'.PROJECT_NAME);
+ 
+ //设置项目控制器目录
+ defined('Controller_PATH') or define('Controller_PATH',PROJECT_PATH.'/Controller/');
+ 
+ //设置项目模型目录
+ defined('Model_PATH') or define('Model_PATH',PROJECT_PATH.'/Model/');
+ 
+ //设置项目模版目录
+ defined('TEMPLATE_PATH') or define('TEMPLATE_PATH',PROJECT_PATH.'/Tpl/');
+ 
+ //设置系统包含目录
+ $system_include = get_include_path();
+ $system_include .= PATH_SEPARATOR.DREAM_CORE_PATH; //包含框架核心类目录
+ $system_include .= PATH_SEPARATOR.Controller_PATH; //包含控制器目录
+ 
+ //设置include包含文件所在的所有目录
+ set_include_path($system_include);
+ 
+ //加载框架方法库
+ include 'DREAM_LIBRARIES_PATH'.'function.inc.php';
+
+ 
+ 
  
  
